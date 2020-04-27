89a3a469c20015baaa03ba4f8f69bf76a011bcb4
syedtasrif <63149669+syedtasrif@users.noreply.github.com>
2020-04-28 01:16:51 +1000

    Create tute7_main.php

---
+++ Tut7/Tute7_Code/tute7_main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>   
+    </head>
+<body>
+    <div class="jumbotron">
+        <div class="container">
+            <h1 align ="center">Course Units</h1>
+        </div>
+    </div>
+    <center>
+     <a href="tute7_view.php" class="btn btn-success" role="button">View Unit details</a>
+      <a href="tute7_manage.php" class="btn btn-primary" role="button">Manage Unit details</a>
+    <br />
+    </center>
+ </body>  
+</html>  
