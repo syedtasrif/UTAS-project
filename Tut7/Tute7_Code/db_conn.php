b7597108c17aab85086eabcc45ba137d07c25dfa
syedtasrif <63149669+syedtasrif@users.noreply.github.com>
2020-04-28 01:16:47 +1000

    Create db_conn.php

---
+++ Tut7/Tute7_Code/db_conn.php
@@ -0,0 +1,9 @@
+<?php
+//connect to mysql
+$mysqli = new mysqli('localhost', 'smat', '555867', 'smat');
+
+if (mysqli_connect_errno()) {
+	    printf("Connect failed: %s\n", mysqli_connect_error());
+	    exit();
+	}
+?>
