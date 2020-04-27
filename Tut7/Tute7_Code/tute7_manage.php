80343a0b74b8630db3ec71d9bb2e3c50236edb23
syedtasrif <63149669+syedtasrif@users.noreply.github.com>
2020-04-28 01:16:55 +1000

    Create tute7_manage.php

---
+++ Tut7/Tute7_Code/tute7_manage.php
@@ -0,0 +1,27 @@
+<?php
+include('db_conn.php'); //db connection
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+     <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- Link to use icon-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- add library-->
+    
+    </head>
+    <body>
+        <div class ="container">
+            <h2 align ="center">Manage Unit details</h2>
+        </div>
+    </body>  
+</html>  
