e10c63c8d8474a0b9f0416cbe75f3bf5f51458c1
syedtasrif <63149669+syedtasrif@users.noreply.github.com>
2020-04-28 23:51:44 +1000

    Create login.php

---
+++ signup-login/login.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>
+            Login Form
+        </title>
+        <link rel="stylesheet" type="text/css" href="CSS/formStyle.css">
+        <body>
+            <div class="loginbox">
+                <img src="Images/avatar.png" class="avatar">
+                <h1>Login Here</h1>
+                <form action="includes/login.inc.php" method="post">
+                    <p>Username</p>
+                    <input type="text" name="mailuid" placeholder="Enter Email Address/Username..">
+                    <p>Password</p>
+                    <input type="password" name="pwd" placeholder="Password">
+                    <input type="submit" name="login-submit" value="Login">
+                    <a href="#">Lost your password?</a><br>
+                    <a href="register.html">Don't have an account?</a>
+                    
+                </form>
+                
+            </div>
+            
+        </body>
+    </head>
+</html>
